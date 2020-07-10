3b7b7087272f3c9f7bf24142f7066980a31391ac
liu <mingjilw023@163.com>
2020-07-10 23:49:26 +0800

    add 15-7

--- 1.py
+++ 1.py
@@ -1,8 +1,9 @@
-file_name = '2.txt'
+file_name = '1.txt'
 file2_name = '3.txt'
-
-with open(file_name,'r',encoding='utf8') as file_object:
-    for i in file_object:
-        with open(file2_name,'a',encoding='utf8') as file2_objiet:
-            file2_objiet.write('$tbr=="' + i.rstrip() + '"' + '||')
-      
+try:
+    with open(file_name,'r',encoding='utf8') as file_object:
+        for i in file_object:
+            with open(file2_name,'a',encoding='utf8') as file2_objiet:
+                file2_objiet.write('$tbr=="' + i.rstrip() + '"' + '||')
+except FileNotFoundError:
+        print('aaa')
