3221565b79c98ee53b1714d665739808b83a64e9
alexliuwei <mingjilw023@163.com>
2020-07-13 11:49:30 +0800

    add

--- 16章/zg.py
+++ 16章/zg.py
@@ -4,7 +4,7 @@ import csv
 zhiwu = {}
 fz_name,zz_name  = [],[]
 zhengzhi_name = ['主任','副主任（主持工作）','科长','党支部书记、所长','副所长（主持党政工作）','所长','院长','党支部书记、院长','副院长（主持党政工作）','党支部书记、主任']
-file_name = 'zg.csv'
+file_name = '../zg.csv'
 with open(file_name,encoding='utf-8') as f:
     readr = csv.reader(f)
     row = next(readr)
@@ -21,7 +21,7 @@ for xm,zw in zhiwu.items():
 print(fz_name)
 print(zz_name)
 
-file_name = '3.txt'
+file_name = '../3.txt'
 with open(file_name,'a') as file_object:
     for name in  zz_name:
         file_object.write('$tbr=="' + name.rstrip() + '"' + '||')
